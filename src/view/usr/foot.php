c09f10ec68c3a5b73bd558bf6b23f123d7f1b489
bgchoi205 <choibg205@naver.com>
2021-07-12 10:37:26 +0900

    작성화면, 수정화면 수정

--- src/view/usr/foot.php
+++ src/view/usr/foot.php
@@ -1,5 +1,5 @@
     </main>
-    <footer class="bottom-bar h-20 bg-black text-white">
+    <footer class="bottom-bar h-20 text-white">
       <div class="container mx-auto h-full flex items-center justify-center">
         <a href="/" class="top-bar__logo flex items-center">
           <span><i class="fas fa-laptop-code"></i></span>
