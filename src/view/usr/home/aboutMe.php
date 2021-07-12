76355e00ed65d58c833a1ecab7f6709b76690376
bgchoi205 <choibg205@naver.com>
2021-07-12 11:11:07 +0900

    여백 수정

--- src/view/usr/home/aboutMe.php
+++ src/view/usr/home/aboutMe.php
@@ -1,5 +1,5 @@
 <?php
-$pageTitleIcon = '<i class="fas fa-list"></i>';
+$pageTitleIcon = '<i class="fas fa-id-card"></i>';
 $pageTitle = "관리자 소개";
 ?>
 <?php require_once __DIR__ . "/../head.php"; ?>
@@ -7,7 +7,9 @@ $pageTitle = "관리자 소개";
 
     <section class="section-about-me mt-3">
         <div class="container mx-auto">
-            고기 좋아하고 풀떼기,해물 싫어하는 초딩입맛입니다.
+            <div class="con-pad">
+                고기 좋아하고 풀떼기,해물 싫어하는 초딩입맛입니다.
+            </div>
         </div>
     </section>
 <?php require_once __DIR__ . "/../foot.php"; ?>
