be67a751b09727125f8d6d659ad27153b771f35e
bgchoi205 <choibg205@naver.com>
2021-07-12 11:01:50 +0900

    aboutMe 페이지 추가

---
+++ src/view/usr/home/aboutMe.php
@@ -0,0 +1,13 @@
+<?php
+$pageTitleIcon = '<i class="fas fa-list"></i>';
+$pageTitle = "관리자 소개";
+?>
+<?php require_once __DIR__ . "/../head.php"; ?>
+<?php require_once __DIR__ . "/../../part/toastUiSetup.php"; ?>
+
+    <section class="section-about-me mt-3">
+        <div class="container mx-auto">
+            고기 좋아하고 풀떼기,해물 싫어하는 초딩입맛입니다.
+        </div>
+    </section>
+<?php require_once __DIR__ . "/../foot.php"; ?>
